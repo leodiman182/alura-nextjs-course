26d51bf3a9cc4e6360e37f9f9d2b7f4eb88ad87c
Leonardo Martins <leonardo.martins@cotefacil.com>
2023-07-20 14:10:59 -0300

    commit inicial - aula 03

---
+++ pages/404.jsx
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link';
+
+export default function FAQPage() {
+  return (
+    <div>
+      <h1>ERRO! 404</h1>
+      <Link href="/">Ir para home</Link>
+    </div>
+  );
+}
---
+++ pages/faq.jsx
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link';
+
+export default function FAQPage() {
+  return (
+    <div>
+      <h1>Página de FAQs</h1>
+      <Link href="/">Ir para home</Link>
+    </div>
+  );
+}
